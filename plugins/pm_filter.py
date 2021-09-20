dfbd871bb58cc9f114a20a17db50e11e3adffcce
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-09-20 10:34:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -54,10 +54,10 @@ async def filter(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"[{get_size(file.file_size)}] {file.file_name}"
+                filename = f"{file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")],
-                    [InlineKeyboardButton(text=f"{file.file_size}",callback_data=f"subinps#{file_id}")]
+                    [InlineKeyboardButton(text=f"[{get_size(file.file_size)}]",callback_data=f"subinps#{file_id}")]
                     )
         else:
             await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
@@ -121,7 +121,7 @@ async def group(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"[{get_size(file.file_size)}] {file.file_name}"
+                filename = f"{file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")],
                     [InlineKeyboardButton(text=f"{file.file_size}",callback_data=f"subinps#{file_id}")]
