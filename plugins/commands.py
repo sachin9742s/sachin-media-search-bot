400568d370499b2c41be62cf57a1434320f5bc79
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-09-20 11:17:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -196,4 +196,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
         ]
         ]
-    await message.reply(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await query.message.edit(text=f"<b>᳃ My Boss : <a href='https://t.me/sachin_official_admin'>Sachin S</a>\n᳃ Language : <code>Python3</code>\n᳃ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\n᳃ Update Group : <a href='https://t.me/KicchaRequest'>Kiccha Request</a>\n᳃ Update Channel : <a href='https://t.me/GD_FILMCLUB'>Kiccha Request Channel</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
