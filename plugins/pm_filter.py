4cd8664e167cbd6ae9142e9249d1f1c3304ca52d
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-09-29 19:38:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 import re
 from pyrogram.errors import UserNotParticipant
 from utils import get_filter_results, get_file_details, is_subscribed, get_poster
-from utils import RATING, GENRES, ABOUT
+import RATING, GENRES, ABOUT
 import random
 BUTTONS = {}
 BOT = {}
